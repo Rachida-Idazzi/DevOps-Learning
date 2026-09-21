15a14f4fa2f3f273c5540c73750b72b96075e4a0
Rachida-Idazzi <idazzirachida.id@gmail.com>
2026-09-21 07:00:30 +0100

    file added: parse-config.sh

--- Bash-scripting/config-parser/parse-config.sh
+++ Bash-scripting/config-parser/parse-config.sh
@@ -1,10 +1,35 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 # ============================================================
-# Script:     parse-config.sh
 # Objective:  Read a KEY=VALUE configuration file and print each key-value pair.
-# Usage:      ./parse-config.sh <file.conf>
 # ============================================================
 
 set -euo pipefail
 
-# TODO: implement
+key_value() {
+
+config_file="$1"
+
+if [[ ! -f "$config_file" ]]; then 
+    echo "Error: file $config_file does not exist!" >&2
+    return 1
+fi
+
+while IFS='=' read r key value; do 
+    #Skip blank lines
+if [[ -z "$key" ]]; then
+    continue
+fi
+
+    #Skip comments
+if [[ "$key" =~ ^[[:space:]]*# ]]; then
+    continue
+fi
+
+echo "Key:$key; Value:$value"
+
+done < "$config_file"
+
+}
+
+key_value "$1"
