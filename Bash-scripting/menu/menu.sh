6b414289b7c4ab6579533f1be10f8ba8ad3f8c3c
Rachida-Idazzi <idazzirachida.id@gmail.com>
2026-09-22 08:18:23 +0100

    file added: menu.sh

--- Bash-scripting/menu/menu.sh
+++ Bash-scripting/menu/menu.sh
@@ -1,10 +1,50 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # ============================================================
-# Script:     menu.sh
-# Objective:  Present an interactive menu of system tasks (disk space, uptime, list users) and run the chosen one.
-# Usage:      ./menu.sh
+# Objective: Present an interactive menu of system tasks:
+#              -disk space
+#              -uptime
+#              -list users
+#             And run the chosen one.
 # ============================================================
 
 set -euo pipefail
 
-# TODO: implement
+show_menu() {
+
+echo "========Interactive menu======="
+echo "1. Disk Space"
+echo "2. Uptime"
+echo "3. List Users"
+echo "4. Quit"
+
+}
+
+while true; do 
+
+    show_menu
+    read -rp echo "Please enter your choice [1-4]:" choice
+    
+    case "$choice" in 
+    1) 
+     echo "===Disk Space==="
+     df -hT
+     ;;
+
+    2)
+      echo "===Uptime==="
+      uptime
+      ;;
+
+    3) 
+     echo "===Users==="
+     who
+     ;;
+     
+
+
+
+
+
+
+done
+
